9018e0138762deb5a9317fa3933d48e3693e04d6
duvansh91 <jaimeduvan1996@gmail.com>
2021-01-30 20:19:40 -0500

    test: Add project tests

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const expect = require('chai').expect
+const app = require('../src/app')
+
+describe('GET /', () => {
+  it('Should return is palidrome', async () => {
+    const response = await request(app).get('/iecho?text=Level')
+    expect(response.status).to.equal(200)
+    expect(response.body).to.deep.equal({ text: 'leveL', palindrome: true })
+  })
+  it('Should return is not palidrome', async () => {
+    const response = await request(app).get('/iecho?text=Hello')
+    expect(response.status).to.equal(200)
+    expect(response.body).to.deep.equal({ text: 'olleH', palindrome: false })
+  })
+  it('Should return not text', async () => {
+    const response = await request(app).get('/iecho?text=')
+    expect(response.status).to.equal(400)
+    expect(response.body).to.deep.equal({ error: 'no text' })
+  })
+})
